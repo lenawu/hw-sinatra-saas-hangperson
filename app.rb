9c447558dca1a91e79d7a48d8e81577b6a0438ae
lenawu <lenawu@berkeley.edu>
2016-02-05 01:33:36 +0000

    guess implemented

--- app.rb
+++ app.rb
@@ -40,6 +40,15 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+	process = @game.guess(letter)
+    rescue ArgumentError
+	flash[:message] = "Invalid guess."
+    else
+	if not process
+	  flash[:message] = "You have already used that letter."
+   	end
+    end 
     redirect '/show'
   end
   
